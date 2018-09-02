67de3e2f6ec410198c24783650503ddbf003427f
smarty1palak <smarty1palak@gmail.com>
2018-09-02 12:58:10 +0530

    Image blur

--- ImageBlur/dataset_generator.cpp
+++ ImageBlur/dataset_generator.cpp
@@ -1,69 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
 
 #define CHANNELS 3
-#define BLUR_SIZE 5
+#define BLUR_SIZE 1
 
 static char *base_dir;
 
-static void compute(unsigned char *out, unsigned char *in,
-                    unsigned int height, unsigned int width) {
+static void compute(unsigned char *out, unsigned char *in, 
+  unsigned int height, unsigned int width) {
+
   for (unsigned int row = 0; row < height; row++) {
     for (unsigned int col = 0; col < width; col++) {
-      int pixVal = 0;
-      int pixels = 0;
-      // Get the average of the surrounding BLUR_SIZE x BLUR_SIZE box
-      for (int blurrow = -BLUR_SIZE; blurrow < BLUR_SIZE + 1; ++blurrow) {
-        for (int blurcol = -BLUR_SIZE; blurcol < BLUR_SIZE + 1;
-             ++blurcol) {
-          int currow = row + blurrow;
-          int curcol = col + blurcol;
-          // Verify we have a valid image pixel
-          if (currow > -1 && static_cast<unsigned int>(currow) < height &&
+      for (int channel=0; channel<CHANNELS; channel++) {
+    
+        int pixVal = 0;
+        int pixels = 0;
+        // Get the average of the surrounding BLUR_SIZE x BLUR_SIZE box
+        for (int blurrow = -BLUR_SIZE; blurrow < BLUR_SIZE + 1; ++blurrow) {
+          for (int blurcol = -BLUR_SIZE; blurcol < BLUR_SIZE + 1; ++blurcol) {  
+
+            int currow = row + blurrow;
+            int curcol = col + blurcol;
+            // Verify we have a valid image pixel
+            if (currow > -1 && static_cast<unsigned int>(currow) < height &&
               curcol > -1 && static_cast<unsigned int>(curcol) < width) {
-            pixVal += in[currow * width + curcol];
-            pixels++; // Keep track of number of pixels in the avg
+
+              pixVal += in[CHANNELS*(currow * width + curcol) + channel];
+              pixels++; // Keep track of number of pixels in the avg
+            }
           }
         }
-      }
 
-      // Write our new pixel value out
-      out[row * width + col] = (unsigned char)(pixVal / pixels);
+        // Write our new pixel value out
+        out[CHANNELS*(row * width + col) + channel] = (unsigned char)(pixVal / pixels);
+      }
     }
   }
 }
 
-static unsigned char *generate_data(const unsigned int y,
-                                    const unsigned int x) {
-  /* raster of y rows
-     R, then G, then B pixel
-     if maxVal < 256, each channel is 1 byte
-     else, each channel is 2 bytes
+static unsigned char *generate_data(const unsigned int y, const unsigned int x) {
+  
+  /*  raster of y rows
+    R, then G, then B pixel
+    if maxVal < 256, each channel is 1 byte
+    else, each channel is 2 bytes
   */
   unsigned int i;
 
   const int maxVal    = 255;
-  unsigned char *data = (unsigned char *)malloc(y * x * 3);
+  unsigned char *data = (unsigned char *)malloc(y * x * CHANNELS);
 
   unsigned char *p = data;
   for (i = 0; i < y * x; ++i) {
     unsigned short r = rand() % maxVal;
     unsigned short g = rand() % maxVal;
     unsigned short b = rand() % maxVal;
-    *p++             = r;
-    *p++             = g;
-    *p++             = b;
+    *p++ = r;
+    *p++ = g;
+    *p++ = b;
   }
   return data;
 }
 
-static void write_data(char *file_name, unsigned char *data,
-                       unsigned int width, unsigned int height,
-                       unsigned int channels) {
+static void write_data(const char *file_name, unsigned char *data,
+  unsigned int width, unsigned int height, unsigned int channels) {
+
   FILE *handle = fopen(file_name, "w");
   if (channels == 1) {
     fprintf(handle, "P5\n");
-  } else {
+  }
+  else {
     fprintf(handle, "P6\n");
   }
+
   fprintf(handle, "#Created by %s\n", __FILE__);
   fprintf(handle, "%d %d\n", width, height);
   fprintf(handle, "255\n");
@@ -74,15 +83,14 @@ static void write_data(char *file_name, unsigned char *data,
   fclose(handle);
 }
 
-static void create_dataset(const int datasetNum, const int y,
-                           const int x) {
+static void create_dataset(const int datasetNum, const int y, const int x) {
 
-  char *input_file_name  = "input.ppm";
-  char *output_file_name = "output.ppm";
+  const char *input_file_name  = "input.ppm";
+  const char *output_file_name = "output.ppm";
 
   unsigned char *input_data = generate_data(y, x);
-  unsigned char *output_data =
-      (unsigned char *)calloc(sizeof(unsigned char), y * x * CHANNELS);
+  unsigned char *output_data = (unsigned char *)calloc(
+    sizeof(unsigned char), y * x * CHANNELS);
 
   compute(output_data, input_data, y, x);
 
@@ -95,15 +103,14 @@ static void create_dataset(const int datasetNum, const int y,
 
 int main() {
 
-  create_dataset(0, 256, 256);
-  create_dataset(1, 512, 512);
-  create_dataset(2, 512, 256);
-  create_dataset(3, 89, 1024);
-  create_dataset(4, 1023, 1024);
-  create_dataset(5, 1023, 1124);
-  create_dataset(6, 1923, 1124);
-  create_dataset(7, 1920, 1124);
+  // create_dataset(0, 256, 256);
+  // create_dataset(1, 512, 512);
+  // create_dataset(2, 512, 256);
+  // create_dataset(3, 89, 1024);
+  // create_dataset(4, 1023, 1024);
+  // create_dataset(5, 1023, 1124);
+  // create_dataset(6, 1923, 1124);
+  // create_dataset(7, 1920, 1124);
   create_dataset(8, 1020, 1024);
-  create_dataset(9, 3020, 124);
-  return 0;
+  // create_dataset(9, 3020, 124);
 }
--- ImageBlur/template.cu
+++ ImageBlur/template.cu
@@ -1,10 +1,42 @@
-
-#define BLUR_SIZE 5
+#include<cuda.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "wb.h"
+#define BLUR_SIZE 1
 
 //@@ INSERT CODE HERE
 
-int main(int argc, char *argv[]) {
+__global__ void blur( float * input, float * output, int  height, int width)
+{
 
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+	if(x<height && y<width)
+	{
+	for(int k=0;k<3;k++)
+	{
+	float sum=0;
+	int count=0;
+	for(int i=x-BLUR_SIZE; i<= x+BLUR_SIZE; i++)
+        {
+		for(int j= y-BLUR_SIZE; j<=y+BLUR_SIZE;j++)
+		{
+			if(i>=0 && i<height && j>=0 && j<width)
+			{
+				count++;
+				sum+=input[3*(i*width+j)+k];
+			}
+		}
+	}
+	output[3*(x*width+y)+k]=sum/count;
+	}
+	}
+	else
+            return ;
+} 
+
+int main(int argc, char *argv[]) {
   int imageWidth;
   int imageHeight;
   char *inputImageFile;
@@ -18,7 +50,7 @@ int main(int argc, char *argv[]) {
   
   /* parse the input arguments */
   //@@ Insert code here
-
+  wbArg_t args = wbArg_read(argc, argv);
   inputImageFile = wbArg_getInputFile(args, 0);
 
   inputImage = wbImport(inputImageFile);
@@ -29,7 +61,7 @@ int main(int argc, char *argv[]) {
   imageHeight = wbImage_getHeight(inputImage);
 
   // Since the image is monochromatic, it only contains only one channel
-  outputImage = wbImage_new(imageWidth, imageHeight, 1);
+  outputImage = wbImage_new(imageWidth, imageHeight, 3);
 
   hostInputImageData  = wbImage_getData(inputImage);
   hostOutputImageData = wbImage_getData(outputImage);
@@ -38,26 +70,32 @@ int main(int argc, char *argv[]) {
 
   wbTime_start(GPU, "Doing GPU memory allocation");
   cudaMalloc((void **)&deviceInputImageData,
-             imageWidth * imageHeight * sizeof(float));
+             3*imageWidth * imageHeight * sizeof(float));
   cudaMalloc((void **)&deviceOutputImageData,
-             imageWidth * imageHeight * sizeof(float));
+             3*imageWidth * imageHeight * sizeof(float));
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
   cudaMemcpy(deviceInputImageData, hostInputImageData,
-             imageWidth * imageHeight * sizeof(float),
+             3*imageWidth * imageHeight * sizeof(float),
              cudaMemcpyHostToDevice);
   wbTime_stop(Copy, "Copying data to the GPU");
 
   ///////////////////////////////////////////////////////
   wbTime_start(Compute, "Doing the computation on the GPU");
+ 
+  dim3 block(32, 32, 1);
+  dim3 grid(imageHeight/32 +1, imageWidth/32 +1, 1);
+
+  blur<<<grid, block>>> (deviceInputImageData, deviceOutputImageData, imageHeight, imageWidth);
+
 
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
   ///////////////////////////////////////////////////////
   wbTime_start(Copy, "Copying data from the GPU");
   cudaMemcpy(hostOutputImageData, deviceOutputImageData,
-             imageWidth * imageHeight * sizeof(float),
+             3*imageWidth * imageHeight * sizeof(float),
              cudaMemcpyDeviceToHost);
   wbTime_stop(Copy, "Copying data from the GPU");
 
